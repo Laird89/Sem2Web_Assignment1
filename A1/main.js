b54c5f9aad42f8b99634ca873dcdcc5a539de5f5
Laird89 <117320855+Laird89@users.noreply.github.com>
2023-12-19 12:10:22 -0500

    Add files via upload

---
+++ A1/main.js
@@ -0,0 +1,42 @@
+//main.js
+
+let classroom = [
+    {
+        firstName: "Chris",
+        lastName: "Laird",
+        gpa: 4.0,
+        courses: [
+            {
+                className: "Java1",
+                grade: 89
+            },
+            {
+                className: "Web Dev",
+                grade: 98.25
+            },
+            {
+                className: "Math",
+                grade: 95.25
+            }
+        ]
+    },
+    {
+        firstName: "Suzana",
+        lastName: "Guleva",
+        gpa: 4.0,
+        courses: [
+            {
+                className: "Math",
+                grade: 90
+            },
+            {
+                className: "Data Comm",
+                grade: 92
+            },
+            {
+                className: "Tech Comm",
+                grade: 97
+            }
+        ]
+    }
+]
